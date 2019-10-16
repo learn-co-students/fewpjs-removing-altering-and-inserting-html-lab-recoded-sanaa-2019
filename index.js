383912e194171e692dd9c016a69eb68d89f48451
Eman Al-zariey <eman_alzariey@yahoo.com>
2019-10-16 03:57:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+
+//remove main with id #main
+
+var removeElement = document.getElementById("main");
+
+removeElement.parentNode.removeChild(removeElement);
+
+//create new header with text node in it
+var newHeader = document.createElement('h1')
+newHeader.id = "victory"
+var node = document.createTextNode("Eman is the champion ^_* !");
+newHeader.appendChild(node);
+document.body.appendChild(newHeader)
