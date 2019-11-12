4eaa95758d912ed9fa65f2bd7d0e80915fed748d
Sarah Alafif <sar1997yah@gmail.com>
2019-11-12 20:33:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ let newHeader=document.createElement('h1');
 newHeader.setAttribute("id","victory");
 
 document.body.appendChild(newHeader);
-document.getElementById("victory").innerHTML="YOUR-NAME is the champion";
+newHeader.innerHTML="YOUR-NAME is the champion";
